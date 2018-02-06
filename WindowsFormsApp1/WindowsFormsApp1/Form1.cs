7da0ff5ef6fd74ff34d417d136ca420eb0000dbf
Luka N Sever <lukasever.56@gmail.com>
2018-02-06 12:03:36 +0100

    progress - interesting bugs

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,21 +12,49 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Pen pen = new Pen(Color.Red);
+        float width = 10;
+        float height = 10;
+        bool is_moving = false;
+        Point start;
+        RectangleF circle = new RectangleF(0,0,0,0);
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
-            float Mouse_Pos_x = e.X;
-            float Mouse_Pos_y = e.Y;
-            float width = 20;
-            float height = 20;
-            Pen pen = new Pen(Color.Red);
-            RectangleF circle = new RectangleF(Mouse_Pos_x-width/2, Mouse_Pos_y-height/2, width, height);
             Graphics g = panel1.CreateGraphics();
+            int Mouse_Pos_x = e.X;
+            int Mouse_Pos_y = e.Y;
+
+            Mouse_Pos_x -= (int)width / 2;
+            Mouse_Pos_y -= (int)height / 2;
+
+            start = new Point(Mouse_Pos_x, Mouse_Pos_y);
+
+            if (e.Button == MouseButtons.Left)
+            {
+                is_moving = true;
+            }
+
+            circle = new RectangleF(start.X, start.Y, width, height);
             g.DrawEllipse(pen, circle);
+            
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            if (is_moving)
+            {
+                width = e.X - start.X;
+                height = e.Y - start.Y;
+                circle = new RectangleF(start.X, start.Y, width, height);
+                g.DrawEllipse(pen, circle);
+            }
         }
     }
 }
