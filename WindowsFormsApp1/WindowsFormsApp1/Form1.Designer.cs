baafa59b814fe8a005a579e907be5d9fce7c77bf
Luka N Sever <lukasever.56@gmail.com>
2018-02-08 09:38:18 +0100

    Custom exception & handler added..better math..actually draws circle from center point (rectangle from center point)

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -80,7 +80,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(447, 281);
+            this.button1.Location = new System.Drawing.Point(447, 255);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -92,7 +92,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 316);
+            this.ClientSize = new System.Drawing.Size(534, 285);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.maskedTextBox2);
             this.Controls.Add(this.maskedTextBox1);
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private Rectangle circle;
+        private RectangleF circle;
         private Point start;
         private Point end;
         private double area;
@@ -31,6 +31,7 @@ namespace WindowsFormsApp1
             circle = new Rectangle(0, 0, 0, 0);
             area = 0;
             circumference = 0;
+
             maskedTextBox1.Clear();
             maskedTextBox2.Clear();
         }
@@ -38,14 +39,32 @@ namespace WindowsFormsApp1
         private void panel1_MouseUp(object sender, MouseEventArgs e)
         {
             Graphics g = panel1.CreateGraphics();
+            RectangleF bounds = new RectangleF(0, 0, 0, 0);
+            bounds = g.VisibleClipBounds; 
             end.X = e.X;
             end.Y = e.Y;
-            circle = new Rectangle(start.X, start.Y, (end.X-start.X), (end.X-start.X));
-            area = Math.Pow(circle.Width/2, 2) * Math.PI;
-            circumference = 2 * circle.Width/2 * Math.PI;
+            float height_and_width = (float) Math.Sqrt(Math.Pow(end.X - start.X, 2) + Math.Pow(end.Y - start.Y, 2));
+
+            circle = new RectangleF(start.X - height_and_width / 2, start.Y - height_and_width / 2, height_and_width, height_and_width);
+            area = Math.Pow(circle.Width / 2, 2) * Math.PI;
+            circumference = 2 * circle.Width / 2 * Math.PI;
             maskedTextBox1.AppendText(circumference.ToString());
             maskedTextBox2.AppendText(area.ToString());
-            g.DrawEllipse(new Pen (Color.Red, 1), circle);
+            g.DrawEllipse(new Pen(Color.Red, 1), circle);
+            
+            try
+            {
+                if (circle.Right > bounds.Right || 0 > circle.Left)
+                 throw new ApplicationException();
+                if (circle.Bottom > bounds.Bottom || 0 > circle.Top)
+                 throw new ApplicationException(); 
+            }
+            catch (ApplicationException)
+            {
+                String exception = "Your Circle is out of Bounds!";
+                String caption = "CIRCLE_EXCEPTION";
+                MessageBox.Show(exception, caption);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
